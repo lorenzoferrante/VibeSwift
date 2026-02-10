cc5c628f24aab7b3822339b02794fed31d6819e6
Lorenzo Ferrante <lorenzoferrante96@gmail.com>
2026-02-10 20:11:58 +0100

    Initial Commit

---
+++ VibeSwift/VibeSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibeSwiftApp.swift
+//  VibeSwift
+//
+//  Created by Lorenzo Ferrante on 10/02/26.
+//
+
+import SwiftUI
+
+@main
+struct VibeSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
